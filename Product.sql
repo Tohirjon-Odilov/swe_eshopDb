fdb4477c35aeacddc01eed6c658ffd7a3220979c
Ozodbek <olimjanovozodbek32@gmail.com>
2024-02-01 18:55:18 +0500

    update files

--- Product.sql
+++ Product.sql
@@ -23,7 +23,7 @@ CREATE TABLE manufacturer(
     id serial primary key,name varchar(255));
 
 create table product_title(
-    id bigserial not null primary key,
+    id serial not null primary key,
     title varchar(255),
-    product_category_id bigint not null
+    product_category_id int not null
 );
