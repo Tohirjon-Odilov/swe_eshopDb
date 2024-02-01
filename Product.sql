65a91968884084d6b74c19f144c2385496e3f505
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-02-01 15:55:10 +0500

    created order details

---
+++ Product.sql
@@ -0,0 +1,9 @@
+`--CREATE TABLE order_details(
+--    id SERIAL,
+--    customer_order_id INTEGER ,
+--    product_id INTEGER ,
+--    price INTEGER ,
+--    price_with_discount INTEGER,
+--    product_amount INTEGER 
+--);
+--ALTER TABLE order_details ADD PRIMARY KEY(id);
